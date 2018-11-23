9aab5f81146d7e94b8a36363373db5f783add131
wang926454 <1107224733@qq.com>
2018-11-23 14:49:59 +0800

    部署到docs文件夹

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var imagemin = require('gulp-imagemin');
 
 // 压缩html
 gulp.task('minify-html', function() {
-    return gulp.src('./public/**/*.html')
+    return gulp.src('./docs/**/*.html')
         .pipe(htmlclean())
         .pipe(htmlmin({
             removeComments: true,
@@ -15,32 +15,32 @@ gulp.task('minify-html', function() {
             minifyCSS: true,
             minifyURLs: true,
         }))
-        .pipe(gulp.dest('./public'))
+        .pipe(gulp.dest('./docs'))
 });
 // 压缩css
 gulp.task('minify-css', function() {
-    return gulp.src('./public/**/*.css')
+    return gulp.src('./docs/**/*.css')
         .pipe(minifycss({
             compatibility: 'ie8'
         }))
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('./docs'));
 });
 // 压缩js
 gulp.task('minify-js', function() {
-    return gulp.src('./public/**/*.js')
+    return gulp.src('./docs/**/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('./docs'));
 });
 // 压缩图片
 gulp.task('minify-images', function() {
-    return gulp.src('./public/images/**/*.*')
+    return gulp.src('./docs/images/**/*.*')
         .pipe(imagemin(
         [imagemin.gifsicle({'optimizationLevel': 3}), 
         imagemin.jpegtran({'progressive': true}), 
         imagemin.optipng({'optimizationLevel': 7}), 
         imagemin.svgo()],
         {'verbose': true}))
-        .pipe(gulp.dest('./public/images'))
+        .pipe(gulp.dest('./docs/images'))
 });
 // 默认任务
 gulp.task('default', [
--- source/README.md
+++
@@ -1,5 +0,0 @@
-# Notes
-
-> NexT is a high quality elegant [Jekyll](https://jekyllrb.com) theme ported from [Hexo NexT](https://github.com/iissnan/hexo-theme-next). It is crafted from scratch, with love.
-
-* 在此基础上修改:[https://github.com/DIYgod/hexo-theme-sagiri](https://github.com/DIYgod/hexo-theme-sagiri)
